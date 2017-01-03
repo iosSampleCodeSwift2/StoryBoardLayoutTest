696445e0ea71a4c0e6d8e0fc81b386213af8a7da
ranokim <daesub0515@gmail.com>
2017-01-04 03:16:13 +0900

    first commit

---
+++ StoryBoardLayoutTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryBoardLayoutTest
+//
+//  Created by Daesub Kim on 2016. 11. 4..
+//  Copyright © 2016년 DaesubKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ StoryBoardLayoutTestTests/StoryBoardLayoutTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  StoryBoardLayoutTestTests.swift
+//  StoryBoardLayoutTestTests
+//
+//  Created by Daesub Kim on 2016. 11. 4..
+//  Copyright © 2016년 DaesubKim. All rights reserved.
+//
+
+import XCTest
+@testable import StoryBoardLayoutTest
+
+class StoryBoardLayoutTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
